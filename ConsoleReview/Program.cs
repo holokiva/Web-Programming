7f972d90c20de3837a5204fb9e77c68cf58ea29a
holokiva <lukaskinvana@gmail.com>
2026-05-05 03:56:40 +0300

    restore ConsoleReview

---
+++ ConsoleReview/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("start: ");
+        int start = int.Parse(Console.ReadLine() ?? "0");
+
+        Console.Write("end: ");
+        int end = int.Parse(Console.ReadLine() ?? "0");
+
+        if (start > end)
+        {
+            int temp = start;
+            start = end;
+            end = temp;
+        }
+
+        for (int i = start; i <= end; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (i % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (i % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
